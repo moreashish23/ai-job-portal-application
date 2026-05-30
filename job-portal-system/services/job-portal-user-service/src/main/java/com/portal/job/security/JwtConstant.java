98e19b48ba974d3bc267fe4a5e8a0fb8b7980fb2
moreashish23 <moreashishshivaji23@gmail.com>
2026-05-30 21:52:08 +0530

    Build All Services

--- job-portal-system/services/job-portal-user-service/src/main/java/com/portal/job/security/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.portal.job.security;
-
-public class JwtConstant {
-
-    public static final String SECRET_KEY = "jqweerttyuiipkjhhfjkdkalkmnzbcbvjfioqojbcjdkndbjsdopqjskfkjnkfbhbhfrbbfjsbfwbfjksnfbfckjdnckjnwk";
-
-}
